b26dc0aa831c57ab408f0febc387999d4e222a5e
Abdulrehman Sajid <96682492+imanibutt@users.noreply.github.com>
2026-06-05 22:08:46 +0500

    Use GTM container for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,17 +63,26 @@ export default function RootLayout({
       </head>
       <body suppressHydrationWarning>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-19FHVZ0JS8"
+          id="google-tag-manager"
           strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W8ZWHD9N');
+            `,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-19FHVZ0JS8');
-          `}
-        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W8ZWHD9N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <div className="bg-mesh" />
         <div className="relative z-10">
           {children}
